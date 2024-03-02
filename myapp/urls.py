b985826a06b6d765706e20376dcd80209d543765
aimaarif <2021ag8104@uaf.edu.pk>
2024-03-01 20:30:31 -0800

    correcting errors in user authentication logic

--- myapp/urls.py
+++ myapp/urls.py
@@ -5,10 +5,9 @@ from django.urls import path
 from .views import login_view, signup_view
 from .views import home_view
 
-urlpatterns = [
-    path('', login_view, name='login'), 
+urlpatterns = [ 
+    path('', login_view, name='login'),
     path('login/', login_view, name='login'),
     path('signup/', signup_view, name='signup'),
     path('home/', home_view, name='home'),
-    # Add other URLs as needed
 ]
--- myapp/views.py
+++ myapp/views.py
@@ -1,8 +1,12 @@
-
-# Create your views here.
 # restaurant_app/views.py
 from django.shortcuts import render, redirect
 from .forms import LoginForm, SignupForm
+from django.contrib.auth import authenticate, login
+from django.contrib import messages
+from django import forms
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+from django.views.decorators.csrf import csrf_protect
 
 def home_view(request):
     return render(request, 'home.html')
@@ -11,21 +15,36 @@ def login_view(request):
     if request.method == 'POST':
         form = LoginForm(request.POST)
         if form.is_valid():
-            # Implement your login logic here
-            return redirect('home')
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password']
+            user = authenticate(request, username=username, password=password)
+            if user is not None:
+                login(request, user)
+                messages.success(request, "You Have Been Logged In!")
+                return redirect('home')
+            else:
+                messages.error(request, "Invalid login credentials. Please try again.")
     else:
         form = LoginForm()
 
     return render(request, 'login.html', {'form': form})
 
+
 def signup_view(request):
-    if request.method == 'POST':
-        form = SignupForm(request.POST)
-        if form.is_valid():
-            # Implement your signup logic here
-            return redirect('home')
-    else:
-        form = SignupForm()
-
-    return render(request, 'signup.html', {'form': form})
-
+	form = SignupForm()
+	if request.method == "POST":
+		form = SignupForm(request.POST)
+		if form.is_valid():
+			form.save()
+			username = form.cleaned_data['username']
+			password = form.cleaned_data['password1']
+			# log in user
+			user = authenticate(username=username, password=password)
+			login(request, user)
+			messages.success(request, ("Username Created"))
+			return redirect('home')
+		else:
+			messages.success(request, ("Whoops! There was a problem Registering, please try again..."))
+			return redirect('signup')
+	else:
+		return render(request, 'signup.html', {'form':form})
